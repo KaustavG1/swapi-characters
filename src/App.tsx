9213bedaef89adfd449f0c9840db3c8d8aaec4cb
Kaustav Ghosh <mail2kg2@gmail.com>
2024-12-16 00:28:23 +0530

    Added favourites page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import Character from "./pages/Character";
+import Favourites from "./pages/Favourites";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/favourites" element={<Favourites />} />
       <Route path=":id" element={<Character />} />
     </Routes>
   );
